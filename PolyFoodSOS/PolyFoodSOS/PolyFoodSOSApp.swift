553d9689701a05df86dbc1c2e889653b4ce5162a
Richard Tham <richardtham@Richards-MacBook-Air.local>
2024-02-03 12:06:40 -0500

    Created project

---
+++ PolyFoodSOS/PolyFoodSOS/PolyFoodSOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolyFoodSOSApp.swift
+//  PolyFoodSOS
+//
+//  Created by Richard Tham on 2024-02-03.
+//
+
+import SwiftUI
+
+@main
+struct PolyFoodSOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
